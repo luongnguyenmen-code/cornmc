c32ffecb9caa3d66237d4459653975c0f8c13d08
luongnguyenmen-code <luongnguyenmen@gmail.com>
2026-06-07 10:48:13 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1040,7 +1040,7 @@ window.switchRankTab = (tabName) => {
     if (activeBoard) activeBoard.classList.remove('hidden');
 
     // 3. Reset style tất cả các nút bấm về màu xám mờ
-    ['money', 'online', 'point', 'kill'].forEach(t => {
+    ['june','donate','money', 'online', 'point', 'kill'].forEach(t => {
         const btn = document.getElementById(`tab-btn-${t}`);
         if (btn) btn.className = 'px-5 py-2.5 rounded-xl font-bold text-sm transition bg-white/5 text-gray-400 border border-gray-700 hover:bg-white/10';
     });
@@ -1050,7 +1050,11 @@ window.switchRankTab = (tabName) => {
     if (activeBtn) {
         if (tabName === 'money') {
             activeBtn.className = 'px-5 py-2.5 rounded-xl font-bold text-sm transition bg-green-600/20 text-green-400 border border-green-500/50 shadow-[0_0_15px_rgba(74,222,128,0.3)]';
-        } else if (tabName === 'online') {
+        }else if (tabName === 'june') {
+           activeBtn.className = 'px-5 py-2.5 rounded-xl font-bold text-sm transition bg-orange-600/20 text-orange-400 border border-orange-500/50 shadow-[0_0_15px_rgba(249,115,22,0.3)]';
+        }else if (tabName === 'donate') {
+            activeBtn.className = 'px-5 py-2.5 rounded-xl font-bold text-sm transition bg-cyan-300/20 text-cyan-400 border border-cyan-500/50 shadow-[0_0_15px_rgba(34,211,238,0.3)]';
+        }else if (tabName === 'online') {
             activeBtn.className = 'px-5 py-2.5 rounded-xl font-bold text-sm transition bg-cyan-600/20 text-cyan-400 border border-cyan-500/50 shadow-[0_0_15px_rgba(34,211,238,0.3)]';
         } else if (tabName === 'point') {
             activeBtn.className = 'px-5 py-2.5 rounded-xl font-bold text-sm transition bg-yellow-600/20 text-yellow-400 border border-yellow-500/50 shadow-[0_0_15px_rgba(250,204,21,0.3)]';
